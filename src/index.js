de3318e7785519fea35d5bb345366d8225bfd9a8
Celin <creptonium@gmail.com>
2018-06-23 22:20:43 +1000

    Call to Youtube API, Building video list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import {API_KEY} from './key';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const API_KEY = 'Your-API-Key';
+        this.state = { videos: [] };
 
-//create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({key: API_KEY, term: 'surboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
-//Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
